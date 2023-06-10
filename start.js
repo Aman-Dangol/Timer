859bc6de34a96f052a5c64d0056a0eac84856b9c
Aman Dangol <amandangol423@gmail.com>
2023-06-10 20:01:34 +0545

    added hour

--- start.js
+++ start.js
@@ -1,16 +1,30 @@
 var min = document.getElementById('minutes') ;
 var sec = document.getElementById('secs');
+var hr = document.getElementById('hr');
+var hrCount=0;
 var minCount =0;
 var secCount = 0;
+hr.innerHTML=0;
 min.innerHTML=minCount;
 sec.innerHTML=secCount;
 function start(){
     secCount++;
-    if(secCount==60){
+    if(secCount==3){
         minCount++;
+        if (minCount==3) {
+            hrCount++;
+            minCount=0;
+            secCount=0;
+        hr.innerHTML=hrCount; 
+        min.innerHTML=minCount;
+        sec.innerHTML=secCount;
+
+        }else
+        {
         secCount=0;
-        min.innerHTML=JSON.stringify(minCount);
-        sec.innerHTML=JSON.stringify(secCount);
+        min.innerHTML=minCount;
+        sec.innerHTML=secCount;
+        }
     }
     else{
         sec.innerHTML=secCount;
